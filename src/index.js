b2425dc6729e2862fcefb243f2c8534d9f85992c
Oleksandr Pavlishchev <velogitara@gmail.com>
2022-06-05 08:55:09 +0300

    3

--- src/index.js
+++ src/index.js
@@ -20,9 +20,11 @@ function onInput(e) {
   let inputValue = e.target.value.trim();
   if (inputValue === '') {
     Refs.countryListRef.innerHTML = '';
+    Refs.countryInfoRef.innerHTML = '';
     return;
   } else if (inputValue.length == 1) {
     Refs.countryListRef.innerHTML = '';
+    Refs.countryInfoRef.innerHTML = '';
   }
 
   fetchCountries(inputValue)
@@ -30,17 +32,20 @@ function onInput(e) {
       if (result.length > 10) {
         Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
       } else if (result.length >= 2 && result.length <= 10) {
+        Refs.countryListRef.innerHTML = '';
+        Refs.countryInfoRef.innerHTML = '';
         Refs.countryListRef.innerHTML = createListMarkUp(result);
       } else if ((result.length = 1)) {
         // const countries = result.map(({ flag, name, capital, population, languages }) => {});
 
         Refs.countryListRef.innerHTML = '';
-        Refs.countryListRef.innerHTML = createCountryCard(result);
+        Refs.countryInfoRef.innerHTML = createCountryCard(result);
       }
     })
     .catch(err => {
       if (inputValue.length > 0) {
         Refs.countryListRef.innerHTML = '';
+        Refs.countryInfoRef.innerHTML = '';
         onError();
       }
     });
@@ -49,7 +54,7 @@ function onInput(e) {
 function createListMarkUp(items) {
   return items
     .map(item => {
-      return `<li><img src="${item.flags.svg}" alt="${item.name.official}" width='30px' height='20px'>
+      return `<li class="country-list__items"><img src="${item.flags.svg}" alt="${item.name.official}" width='30px' height='20px'>
  <span>${item.name.official}</span></li>`;
     })
     .join('');
@@ -59,11 +64,17 @@ function createCountryCard(items) {
   return items
     .map(item => {
       return `<div>
-        <p><img src="${item.flags.svg}" alt="${item.name.official}" width='30px' height='20px'>
+        <p class="country-info__line"><img src="${item.flags.svg}" alt="${
+        item.name.official
+      }" width='30px' height='20px'>
         ${item.name.official}</p>
-        <p> Capital: ${item.capital}</p>
-        <p> Population: ${item.population}</p>
-        <p> languages: ${Object.values(item.languages).join(', ')}</p>
+        <p class="country-info__line"> <span class="keyName">Capital</span>: ${item.capital}</p>
+        <p class="country-info__line"> <span class="keyName">Population</span>: ${
+          item.population
+        }</p>
+        <p class="country-info__line"> <span class="keyName">languages</span>: ${Object.values(
+          item.languages,
+        ).join(', ')}</p>
     </div>`;
     })
     .join('');
